1e531c5e5b8c002d789b0f0966d2ec76ed5a6841
Josias <87546094+Josyas@users.noreply.github.com>
2021-09-29 05:20:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercícios_Estrutura_Condicional_03 {
+    class Program {
+        static void Main(string[] args) {
+            double A, B;
+            
+        string[] vet = Console.ReadLine().Split(' '); 
+        A = double.Parse(vet [0]);
+        B = double.Parse(vet [1]);
+
+        if (A % B == 0 || B % A == 0) {
+                Console.WriteLine("São Multiplos");
+        } else {
+                Console.WriteLine("Não são Multiplos");
+         }
+            
+
+        }
+    }
+}
